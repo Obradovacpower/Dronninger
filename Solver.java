0df79f6b4ed57ad4a034d7f671f49bace936d320
Ermin Rayquaza Obradovac <33249069+Obradovacpower@users.noreply.github.com>
2017-10-31 12:51:49 +0100

    Add files via upload

---
+++ Solver.java
@@ -0,0 +1,29 @@
+public class Queen {
+    private int noOfQueeens;
+    private int[] queens;
+    private int noOfSolutions;
+
+    public static void main(String[] args) {
+
+    }
+    public void findAllSolutions(int noOfQueeens){
+
+    }
+
+    private void positionQueens(int row){
+        positionQueens();
+    }
+
+    private boolean legal(int row,int col){
+
+        return false;
+    }
+
+    private void printSolution(){
+
+    }
+
+    private String convert(int row, int col){
+    return "Ahmad";
+    }
+}
